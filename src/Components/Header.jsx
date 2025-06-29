e1020ac51fafdfded018b0f06e4d5a273fbf248d
bhavy-sharma <bhavymay18@gmail.com>
2025-06-30 00:29:04 +0530

    fix Header in Production

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import twitter from '../assets/twitter.png';
+import linkedin from '../assets/linkedin.png';
+import insta from '../assets/insta.png';
+import github from '../assets/github.png';
+import logo from '../assets/Logo.png';
+
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -10,7 +16,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        <img src="../assets/Logo.png" alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
         <span className="brand">Navokta</span>
       </div>
 
@@ -20,19 +26,19 @@ const Header = () => {
         <NavLink to="/Story" onClick={() => setMenuOpen(false)}>Stories</NavLink>
         <NavLink to="/Contact" onClick={() => setMenuOpen(false)}>Contact</NavLink>
         <div className="mobile-socials">
-          <a href="https://www.linkedin.com/company/navokta/" target='_blank'><img src="../assets/linkedin.png" alt="LinkedIn" /></a>
-          <a href="https://x.com/navokta" target='_blank'><img src="../assets/twitter.png" alt="Twitter" /></a>
-          <a href="https://github.com/navokta/" target='_blank'><img src="../assets/github.png" alt="GitHub" /></a>
-          <a href="https://www.instagram.com/navokta/" target='_blank'><img src="../assets/insta.png" alt="Instagram" /></a>
+          <a href="https://www.linkedin.com/company/navokta/" target='_blank'><img src={linkedin} alt="LinkedIn" /></a>
+          <a href="https://x.com/navokta" target='_blank'><img src={twitter} alt="Twitter" /></a>
+          <a href="https://github.com/navokta/" target='_blank'><img src={github} alt="GitHub" /></a>
+          <a href="https://www.instagram.com/navokta/" target='_blank'><img src={insta} alt="Instagram" /></a>
         </div>
       </nav>
 
       <div className="header-right">
         <div className="desktop-socials">
-          <a href="https://www.linkedin.com/company/navokta/" target='_blank'><img src="../assets/linkedin.png" alt="LinkedIn" /></a>
-          <a href="https://x.com/navokta" target='_blank'><img src="../assets/twitter.png" alt="Twitter" /></a>
-          <a href="https://github.com/navokta/" target='_blank'><img src="../assets/github.png" alt="GitHub" /></a>
-          <a href="https://www.instagram.com/navokta/" target='_blank'><img src="../assets/insta.png" alt="Instagram" /></a>
+          <a href="https://www.linkedin.com/company/navokta/" target='_blank'><img src={linkedin} alt="LinkedIn" /></a>
+          <a href="https://x.com/navokta" target='_blank'><img src={twitter} alt="Twitter" /></a>
+          <a href="https://github.com/navokta/" target='_blank'><img src={github} alt="GitHub" /></a>
+          <a href="https://www.instagram.com/navokta/" target='_blank'><img src={insta} alt="Instagram" /></a>
         </div>
 
         <button className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
